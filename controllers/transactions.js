17e13584fe4e9042eb4ee094e59ff3383d3ca651
Rammurthy Mudimadugula <rammurthy@unbundled.io>
2018-10-24 00:10:09 -0500

    added request body validation for post api

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Joi = require('joi');
 const transactions = require('../models/transactions');
 
 /**************************************************************************
@@ -26,19 +27,43 @@ exports.post = function (request, response) {
 	let body = request.body;
 
 	if (body && body.length) {
-		transactions.post(body, (err, res) => {
-			if (err) {
-				response.status(500).send({msg: 'Internal Server Error'});
-			} else {
-				transactions.get(1, (err, data) => {
-					if (err) {
-						response.status(500).send({msg: 'Internal Server Error'});
-					} else {
-						response.status(201).send(data);
-					}
-				})
+
+		const schema = Joi.object().keys({
+			user_id: Joi.string().required(),
+			trans_id: Joi.string().required(),
+			name: Joi.string().required(),
+			amount: Joi.number().required(),
+			date: Joi.date().required()
+		});
+
+		let obj = {};
+
+		body.forEach(function (t,c) {
+			const result = Joi.validate(t, schema);
+
+			if (result.error) {
+				obj['transaction'+(c+1)] = result.error.details;
 			}
 		})
+
+		if (Object.keys(obj) && Object.keys(obj).length) {
+			response.status(400).send(obj);
+		} else {
+			transactions.post(body, (err, res) => {
+				if (err) {
+					response.status(500).send({msg: 'Internal Server Error'});
+				} else {
+					transactions.get(1, (err, data) => {
+						if (err) {
+							response.status(500).send({msg: 'Internal Server Error'});
+						} else {
+							response.status(201).send(data);
+						}
+					})
+				}
+			})
+		}
+
 	} else {
 		response.status(400).send({msg: 'Bad Request Error'});
 	}
